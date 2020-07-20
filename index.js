35414eb9c7676e08bd7ad0ee36b781031be4b8c4
Diego de Leon <dienrique24@hotmail.com>
2020-07-19 23:11:05 -0500

    Mobile Layout - added menu

--- index.js
+++ index.js
@@ -15,4 +15,13 @@ $("#btn-contact").click((e)=>{
 
 $("#p-carousel").carousel({
     interval: false,
-})
+});
+
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  
+  document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
